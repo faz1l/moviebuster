53fade5a61232486b7ad1d04826bd21c8e2b39fc
Naum Naumov <johnparker@Naums-MacBook-Air.local>
2020-02-28 14:00:51 +0300

    Updates

---
+++ precache-manifest.fca7480ddc9e6d5d343b4f01e852f9b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d87f1821f5f2e0731141ce5b3a834ec9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b358008a8446d594b647",
+    "url": "/static/css/main.069965bb.chunk.css"
+  },
+  {
+    "revision": "7f6cc0bde7eeda13c0a3",
+    "url": "/static/js/2.7b226569.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.7b226569.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b358008a8446d594b647",
+    "url": "/static/js/main.b62e62e6.chunk.js"
+  },
+  {
+    "revision": "7e6d8c24c5d3b82fe6be",
+    "url": "/static/js/runtime-main.6f88f9a7.js"
+  }
+]);
