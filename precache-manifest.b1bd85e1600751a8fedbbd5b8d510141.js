f3df60e30feeca9efb520043a4b86ec9f594cf82
Naum Naumov <johnparker@Naums-MacBook-Air.local>
2020-02-28 13:52:55 +0300

    Updates

---
+++ precache-manifest.b1bd85e1600751a8fedbbd5b8d510141.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc204935721106efa3878454625b9b71",
+    "url": "/moviebuster/index.html"
+  },
+  {
+    "revision": "92da0cd8ecf01faa67a0",
+    "url": "/moviebuster/static/css/main.069965bb.chunk.css"
+  },
+  {
+    "revision": "89ec6ffa590e8f077910",
+    "url": "/moviebuster/static/js/2.77bb83dd.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/moviebuster/static/js/2.77bb83dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92da0cd8ecf01faa67a0",
+    "url": "/moviebuster/static/js/main.271c7d0b.chunk.js"
+  },
+  {
+    "revision": "7185012bde1e2873b72a",
+    "url": "/moviebuster/static/js/runtime-main.d2503a61.js"
+  }
+]);
